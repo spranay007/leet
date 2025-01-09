b72dab43a0ff3b583276ad557c583dfad74fae61
Pranay Saha <spranay007@yahoo.in>
2025-01-08 23:43:34 -0500

    Time: 1 ms (4.93%) | Memory: 9.3 MB (7.82%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> myVec(n);
+        int result = 0;
+        for(int i = 0; i < n ; i++){
+            myVec[i] = start + 2*i;
+            result ^= myVec[i];
+        }
+        return result;
+    }
+};
